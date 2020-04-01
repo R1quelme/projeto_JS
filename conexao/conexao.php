f24102cd5279179bfa0dc5b8d6153dbfe5abb57b
R1quelme <53120281+R1quelme@users.noreply.github.com>
2020-03-31 21:18:38 -0300

    Delete conexao.php

--- conexao/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conexao = new mysqli("localhost", "Riquelme_admin", "12345678", "curso_alura_js");
-
-if($conexao->connect_errno) {
-    echo "<p>Encontrei um erro $conexao->errno --> 
-    $conexao->connect_error</p>";
-    die();
-}
